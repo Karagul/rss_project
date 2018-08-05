55f31f91ae53dc1645646d5b16ee427f5bfeaf6f
Michal Wodynski <mikelwatersky@gmail.com>
2018-08-05 20:34:46 +0200

    Add changes to databse beacuse some of currency has longer precision

--- rss_project/apps/rss_reader/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Currency(models.Model):
-    value = models.DecimalField(decimal_places=4, max_digits=6)
-    name = models.CharField(max_length=3)
-    created = models.DateTimeField()
