89f85bb22121b507b6256bf12a8312a1bb054900
Michal Wodynski <mikelwatersky@gmail.com>
2018-08-05 09:54:53 +0200

    Add django_extensions to have jupyter in django

--- rss_project/rss_project/settings.py
+++ rss_project/rss_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.rss_reader'
+    'django_extensions',
+    'apps.rss_reader',
 ]
 
 MIDDLEWARE = [
